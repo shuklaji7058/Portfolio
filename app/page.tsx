06683659f335d5408f3329dfb58bb658d9fdd54c
Anish Shukla <shuklaanish040@gmail.com>
2024-10-19 21:03:01 +0530

    react

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 
 const page = () => {
   return (
     <div>
       <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
-          <h1 className="text-white">Hello</h1>
+          <FloatingNav navItems={[{ name: "Home" }]} />
           <Hero />
+          <Grid />
         </div>
       </main>
     </div>
