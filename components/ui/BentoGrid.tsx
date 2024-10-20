ba4036aea4fb636051496ae6b5c5544487ac3bc1
Anish Shukla <shuklaanish040@gmail.com>
2024-10-20 12:41:17 +0530

    next

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "shuklaanish040@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
@@ -161,16 +161,11 @@ export const BentoGridItem = ({
           )}
           {id === 6 && (
             <div className="mt-5 relative">
-              {/* button border magic from tailwind css buttons  */}
-              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
-              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
-              {/* add handleCopy() for the copy the text */}
               <div
                 className={`absolute -bottom-5 right-0 ${
                   copied ? "block" : "block"
                 }`}
               >
-                {/* <img src="/confetti.gif" alt="confetti" /> */}
                 <Lottie options={defaultOptions} height={200} width={400} />
               </div>
 
